4378e5a87e83a5c70a6e2427e5b57f03299a464c
PY <p.tanguy666@laposte.net>
2022-01-04 13:52:29 +0100

    tp1 v2

--- tp1/app.js
+++ tp1/app.js
@@ -4,36 +4,49 @@
  * En sortant de l'input (événement blur), on valide la valeur et on la met à jour dans le tableau
  */
 
+
+ addTable();
+
 function addTable() {
     const myTableDiv = document.getElementById("root");
-  
-    const table = document.createElement('TABLE');
-    table.id = "myTable";
-    table.border = '1';
-  
-    const tableBody = document.createElement('TBODY');
-    table.appendChild(tableBody);
-  
-    for (let i = 0; i < 5; i++) {
-      const tr = document.createElement('TR');
-      tableBody.appendChild(tr);
-  
-      for (let j = 0; j < 5; j++) {
-        const td = document.createElement('TD');
-        tr.appendChild(td);
-        td.id = "myCell";
-        td.width = '25';
-        //td.contentEditable = true; //Somewhat rare because somewhat unknown.
-        const text = document.createTextNode("0");
-        td.addEventListener("blur", handleInputBlur);
-        td.appendChild(text);
-        td.addEventListener("click", handleClicks);
-      }
-    }
+    const table = Table();
     myTableDiv.appendChild(table);
   }
 
-  addTable();
+
+  function Table(nbLines = 10, nbColumns = 10){
+    const tableBody = document.createElement('TBODY');
+    tableBody.id = "myTable";
+    tableBody.border = '1';
+    for (let i = 0; i < nbLines; i++) {
+      const tr = Row(i, nbColumns);
+      tableBody.appendChild(tr);
+    }
+    return tableBody;
+  }
+
+  function Row(lineNum, nbColumns = 5){
+    const tr = document.createElement('TR');
+
+    for (let j = 0; j < nbColumns; j++) {
+     const td = Column("Case " + lineNum + " " + j);
+     tr.appendChild(td);
+    }
+    return tr
+  }
+
+  function Column(textContent){
+    const td = document.createElement('TD');
+    td.id = "myCell";
+    td.width = '75';
+    //td.contentEditable = true; //Somewhat rare because somewhat unknown.
+    const text = document.createTextNode(textContent);
+    td.addEventListener("blur", handleInputBlur);
+    td.appendChild(text);
+    td.addEventListener("click", handleClicks);
+    return td
+  }
+
   function handleClicks(event){
     const td = event.currentTarget;
     const input = document.createElement("input");
@@ -52,37 +65,3 @@ function addTable() {
     td.replaceChild(text, input);
     td.addEventListener('click', handleClicks);
   }
-//   function functionOnFocus() 
-//     {
-//       this.contentEditable = true;
-//       console.log("focused");
-//     }
-//     function functionOnblur() 
-//     {
-//       this.contentEditable = false;
-    // }
-//   function clickCell() {
-//     const row = document.getElementById('myTable').rows;
-//     for (const i = 0; i < row.length; i++) {
-//         for (const j = 0; j < row[i].cells.length; j++ ) {
-//                     console.log(row[i].cells[j].innerHTML);
-//                     row[i].cells[j].addEventListener('click', function(cell){
-//                         const inputValue = window.prompt("Enter new cell value");
-//                         cell.innerText = inputValue;
-//             })
-
-//         }
-//     }
-// }
-// clickCell();
-
-// const cells = document.querySelectorAll("td");
-
-// for (const cell of cells) {
-//   cell.addEventListener('click', changeValue);
-// }
-
-// function changeValue(){
-//     const inputValue = window.prompt("Enter new cell value");
-//     this.textContent = inputValue;
-// }
