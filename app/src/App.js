6973d76e99f8499bdd4498eca9285b27427b97d4
PY <p.tanguy666@laposte.net>
2022-01-05 13:41:53 +0100

    experiments on buttons

--- app/src/App.js
+++ app/src/App.js
@@ -8,16 +8,31 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Button
+          variant="round"
           title="Button 1"
           style={{ color: "red", backgroundColor: "blue" }}
+          onClick={function handleClick() 
+            {alert("6")}}
         />
         <Button
+          variant="squared"
           title="Button 2"
-          style={{ color: "red", backgroundColor: "yellow" }}
+          style={{ color: "green", backgroundColor: "yellow" }}
+          onClick={function handleClick() 
+            {alert("7")}}
         />
         <Button
+          variant="rounded"
           title="Button 3"
+          style={{ color: "blue", backgroundColor: "green" }}
+          onClick={function handleClick() 
+            {alert("8")}}
+        />
+        <Button
+          title="Button 4"
           style={{ color: "red", backgroundColor: "green" }}
+          onClick={function handleClick() 
+            {alert("9")}}
         />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- app/src/components/lib/Button.js
+++ app/src/components/lib/Button.js
@@ -1,5 +1,32 @@
-function Button(props) {
-  return <button style={props.style}>{props.title}</button>;
+function Button({variant = "squared", style = {}, title, onClick}) {
+  //if(!props.style) props.style ={};
+  //props.style = props.style ? props.style.style : {};
+  //props.style = props.style ?? {};
+  //props.style ??={};
+  //const {variant, style, onClick, title} = props;
+
+  switch (variant) {
+    case 'squared' :
+      style.borderRadius=0;
+      break;
+
+    case 'rounded' :
+      style.borderRadius=5;
+      break;
+    
+    case  'round' :
+      style.borderRadius="50%";
+      break;
+      
+    default:
+      style.borderRadius="27%";
+      break;
+  }
+  return(
+    <button onClick={onClick} style={style}>
+    {title}
+  </button>
+  );
 }
 
 export default Button;
